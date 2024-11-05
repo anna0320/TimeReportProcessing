0d095f7ec05c4a5fd4d109dc91b64856ad04fb91
anna0320 <36837053+anna0320@users.noreply.github.com>
2024-11-05 18:42:02 +0300

    script cganged

--- TimeReportProcessing/wwwroot/script.js
+++ TimeReportProcessing/wwwroot/script.js
@@ -34,7 +34,7 @@ async function addTask(task) {
 function formatTime(totalMinutes) {
     const hours = Math.floor(totalMinutes / 60);
     const minutes = totalMinutes % 60;
-    return ${ String(hours).padStart(2, '0') }:${ String(minutes).padStart(2, '0') };
+    return `${ String(hours).padStart(2, '0') }:${ String(minutes).padStart(2, '0') }`;
 }
 
 function renderTasks(tasks) {
